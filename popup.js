1796365db8a47e1c323b9edeed79b90d27eb7a29
lmieu <metawere@gmail.com>
2018-06-06 01:00:20 -0400

    4. Layer Logic

--- popup.js
+++ popup.js
@@ -5,3 +5,16 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
+
+// on click of button on toolbar button, changes background color of 
+// website to designated 'color' value
+// does so by way of programmatic injection, not code injection
+// tabs.executeScript injects js/css code into a page
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
